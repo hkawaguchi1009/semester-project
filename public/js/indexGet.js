46427441d3fed7eee6fec9c87bf8370e4ba2a864
hkawaguchi1009 <44032593+hkawaguchi1009@users.noreply.github.com>
2018-12-13 04:28:16 -0600

    Update indexGet.js
    Maybe I don't need this anymore

--- public/js/indexGet.js
+++ public/js/indexGet.js
@@ -7,66 +7,46 @@ Inputs are going to be date, text input, and also images. (Skip images for this 
 
 /*
 To do;
-Use responsive design.
-Layout the design of you pages using the Bootstrap grid system to provide for layout on a desktop unit >= 992 p,  768 px <= tablet < 992 px, and phones < 768.
-Must use at least 1 bootstrap UI component. (See https://getbootstrap.com/docs/4.1/components/alerts/) for examples.)
-Navigate between each page. (Easiest way is to use Bootstrap.)
-Use jQuery to validate your data.
-Post your data to http://mcs.drury.edu so that you can verify your data input form works.
-Use jQuery to hard code your initial example data on summary pages.
-When finished merge milestone 4 into the development branch of your repo. Do not delete the Milestone 4 branch.
-Merge the development branch into your master branch.  Do not delete the development branch.
+Add additional functions one at a time using branches from Milestone5 on your Github repo.  Check each out to work on them locally and when completed push the code back to the Github repo and merge the feature branch into the Milestone5 branch.  For example, the first feature branch should probably be setting your project up to use a Node.js web server that serves your static pages.  (Follow the example in the book for the Participation Activity 11.3.1.)  Add additional features from there.  See the Milestone5 branch of my Github repo at https://github.com/ssigman/EMS-Drury for more examples.
+Modify your app to use a MongoDB database.  The app should be able to read data from the MongoDB database and add new records to the MongoDB database.
 */
 
 
-function validate(event){
-    var date = document.getElementById("date");
-    var title = document.getElementById("title");
-    var contents = document.getElementById("contents");
-    if (date.value == ""){
-        date.style.backgroundColor = "Orange";
-        event.preventDefault();
-    }
-    if (title.value.length<=0){
-        title.style.backgroundColor = "Orange";
-        event.preventDefault();
-    }
-    if (contents.value.length<=0){
-        contents.style.backgroundColor = "Orange";
-        event.preventDefault();
-    }
+
+
+
+function getDiary(){
+    var html = "";
+    var contents = $("#contents");
+    var date = $("#date");
+    var title = $("#title");
+    var contents = $("contents");
+   $.get("/api/diary", function(diary) {
+      diary.forEach(function(diary) {
+          title.html(diary.title);
+         contents.html(diary.contents);
+          date.html(diary.date);
+      });
+   });
 }
 
-function getAjax(){
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET","http://mcs.drury.edu");
-    xhr.responseType = 'json';
-    xhr.send();
-}
 
-/*function getData(response){
-    var response = response;
-    var date = response.date
-    var title = response.title;
-    var contents = response.contents;
-    var page2 = "";
-    page2 += date;
-    page2 += title;
-    page2 += contents;
-    document.getElementById("page1") = page2.innerHTML;
-}*/
+var $list = $("ul");
+   $.get("/api/diary", function(diary) {
+      diary.forEach(function(diary) {
+         $list.append("<li>" + diary.title + " - " + 
+            diary.date + "</li>");
+      });
+   });
+
+
 
 window.onload = function(){
-    var widget = document.getElementById("formWidget");
-    
-    var xhr = new XMLHttpRequest(); 
-    
-    xhr.onload = function(){
         
-    getAjax();
+    
 };
     
-}
+
 
 /*var getWidget = document.getElementById("formGet");
     var page2 = "";
